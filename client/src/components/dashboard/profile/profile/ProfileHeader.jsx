5be45aea955199d9b7ba16dc8541291797ea1eb3
Mini-Macro <raghavmundhra.edu@gmail.com>
2025-05-08 21:57:16 +0530

    Update button styling in ProfileHeader for improved UI consistency

--- client/src/components/dashboard/profile/profile/ProfileHeader.jsx
+++ client/src/components/dashboard/profile/profile/ProfileHeader.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const ProfileHeader = ({ user, openModal }) => (
   <div className="flex justify-end mb-4">
     <button
-      className="bg-blue-500 px-4 py-2 text-white font-semibold border rounded-lg"
+      className="bg-blue-50 text-black px-4 py-2 rounded-lg shadow transition-transform duration-200 ease-in-out hover:scale-110 hover:shadow-md"
       onClick={openModal}
     >
       Create/Edit Profile
